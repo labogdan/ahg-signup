97a54f57584f4dae8ad2c3db36f9bba5de795130
Andy <andy@Andys-MacBook-Pro.local>
2022-07-27 13:00:40 -0400

    modifing for heroku

--- app.js
+++ app.js
@@ -21,5 +21,12 @@ app.use((req, res, next) => {
 app.use('/feed', feedRoutes);
 app.use(express.static(path.resolve(__dirname, "./frontend/build")));
 
+app.get("*", (req, res) => {
+    let url = path.join(__dirname, '../frontend/build', 'index.html');
+    if (!url.startsWith('/app/')) // since we're on local windows
+        url = url.substring(1);
+    res.sendFile(url);
+});
+
 
 app.listen(PORT);
