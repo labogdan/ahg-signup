da91197e9c89dce332b49f81108f2e556fb488a4
Andy <andy@Andys-MacBook-Pro.local>
2022-08-03 09:38:27 -0400

    error handling

--- controllers.js
+++ controllers.js
@@ -1,24 +1,25 @@
-//const fetch = require('node-fetch'); // import node-fetch (enables the fetch API to be used server-side)
-const { Pool } = require('pg'); // import node-postgres
+const { Pool } = require('pg');
 
 const DB_URL = process.env.DATABASE_URL || 'postgres://yhaldrjyvtauiy:cfe98df02a0874281a267a3b58d669959fd1aaf96e31b4580aa42bc9d5618f9b@ec2-18-214-35-70.compute-1.amazonaws.com:5432/d882a6s32l45oe';
 
-const pool = new Pool({ // create connection to database
+const pool = new Pool({
     connectionString: DB_URL,
     ssl: {
-        rejectUnauthorized: false // don't check for SSL cert
+        rejectUnauthorized: false
     }
 });
 
 exports.getUsers = (req, res, next) => {
 
     const getString = 'SELECT * FROM users ORDER BY id DESC';
-    pool.query(getString) // send query to select all rows from the 'my_activities' table
+    pool.query(getString)
         .then(results => {
             let users = results.rows;
             res.json({ users })
         })
-        .catch(err => console.log(err));
+        .catch(err => { return res.status(400).send({
+            message: err
+         })});
 
     /*res.status(200).json({
         users: [
@@ -43,18 +44,10 @@ exports.createUser = (req, res, next) => {
     const user = [ req.body ]
     const addString = 'INSERT INTO users (first_name, last_name, age, grade, kroger_participate, kroger_enrolled, volunteer_positions, volunteer_other, parent_name, email_address, is_adult_leader, paypal_address, total_cost) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)';
 
-    console.log(user);
-
     pool.query(addString, user[[0]])
         .then(result => res.json(result))
-        .catch(err => console.log(err));
+        .catch(err => { return res.status(400).send({
+            message: err
+        })});
 
-    // Create post in db
-    /*const title = req.body.title;
-    const content = req.body.content;
-
-    res.status(201).json({
-        message: 'Post created successfully!',
-        post: { id: new Date().toISOString(), title: title, content: content }
-    });*/
 };
